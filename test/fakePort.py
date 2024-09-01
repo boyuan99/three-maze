53fdb169c8ed3ec59ea4020ba5253404958f73d2
Bo Yuan <93609557+boyuan99@users.noreply.github.com>
2024-08-31 23:30:45 -0500

    Add python test
    Use python to generate fake port data to feed in maze

---
+++ test/fakePort.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, jsonify, request
+from flask_socketio import SocketIO
+import random
+import time
+import threading
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+# Global variables
+latest_data = {"x": 0, "z": 0}
+is_generating = False
+generation_thread = None
+
+def generate_sensor_data():
+    global latest_data, is_generating
+    while is_generating:
+        x = random.uniform(-1, 1)
+        z = random.uniform(-1, 1)
+        latest_data = {"x": x, "z": z}
+        socketio.emit('sensor_data', latest_data)
+        time.sleep(0.05)  # 20 times per second
+
+@app.route('/')
+def index():
+    return render_template('test_pyport.html')
+
+@app.route('/data')
+def get_data():
+    return jsonify(latest_data)
+
+@app.route('/toggle_generation', methods=['POST'])
+def toggle_generation():
+    global is_generating, generation_thread
+    is_generating = not is_generating
+
+    if is_generating and (generation_thread is None or not generation_thread.is_alive()):
+        generation_thread = threading.Thread(target=generate_sensor_data)
+        generation_thread.start()
+
+    return jsonify({"is_generating": is_generating})
+
+@socketio.on('connect')
+def test_connect():
+    print('Client connected')
+    socketio.emit('generation_status', {"is_generating": is_generating})
+
+@socketio.on('disconnect')
+def test_disconnect():
+    print('Client disconnected')
+
+if __name__ == '__main__':
+    socketio.run(app, debug=True, port=8765)
