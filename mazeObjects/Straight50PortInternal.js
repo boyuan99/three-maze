45f4d56adad91095060075ca9eb4aa5024be5137
Bo Yuan <93609557+boyuan99@users.noreply.github.com>
2024-09-04 22:30:48 -0500

    Update Straight50PortInternal.js

--- mazeObjects/Straight50PortInternal.js
+++ mazeObjects/Straight50PortInternal.js
@@ -7,7 +7,7 @@ import RAPIER from '@dimforge/rapier3d-compat';
 async function init() {
     // Scene setup
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(renderer.domElement);
