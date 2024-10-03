be543a5770c2bba381ad3325f412297243a8997d
Bo Yuan <boyuan1021@gmail.com>
2024-10-03 11:51:30 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve } from 'path'
+import {resolve} from 'path'
+import * as path from "node:path";
 
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    }
+  },
   server: {
     proxy: {
       '/api': {
