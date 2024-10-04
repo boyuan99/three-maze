7df5929e0c65c2f17384363fd8e771786f289e73
Bo Yuan <boyuan1021@gmail.com>
2024-10-04 17:14:17 -0500

    Add serial sensor method to backend

--- backend/app.py
+++ backend/app.py
@@ -19,6 +19,18 @@ def get_rotation():
     }
     return jsonify(rotation)
 
+@app.route('/api/sensor_data')
+def get_sensor_data():
+    data = {
+        'x': random.uniform(-1, 1),                   # Position x
+        'y': random.uniform(-1, 1),                   # Position y
+        'vx': random.uniform(-1, 0),                    # Velocity x
+        'vy': random.uniform(-1, 0),                    # Velocity y
+        'angle': random.uniform(0, 0.2 * 3.1415926),      # Angle in radians
+        'angular_velocity': random.uniform(-0.01, 0.01),  # Angular velocity
+    }
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
